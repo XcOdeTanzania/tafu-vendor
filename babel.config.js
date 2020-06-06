1a41a14bf228f8414eadbbd1a6c90e06fdd3c49f
admin <kalrobbynson@gmail.com>
2020-06-06 20:12:22 +0300

    project config

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,33 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          alias: {
+            assets: './src/assets',
+            styles:'./src/styles',
+            utils:'./src/utils',
+            screens: './src/screens',
+            navigation: './src/navigation',
+            components: './src/components',
+            state: './src/state',
+          },
+        },
+      ],
+    ],
   };
 };
