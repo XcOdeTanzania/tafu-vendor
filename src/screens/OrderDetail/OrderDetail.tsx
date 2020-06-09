17cdc2e522670be41f95aea64b054871f5f63d37
admin <kalrobbynson@gmail.com>
2020-06-09 08:51:53 +0300

    order details

--- src/screens/OrderDetail/OrderDetail.tsx
+++ src/screens/OrderDetail/OrderDetail.tsx
@@ -1,18 +1,68 @@
 // import strings from "./strings";
 import styles from "./styles";
 import React, { Component } from "react";
-import { Platform, Text, View } from "react-native";
-import { Button, Icon } from "react-native-elements";
+import { Text, View } from "react-native";
+import { Button, Card, Divider } from "react-native-elements";
+import ProfileItem from "components/items/profile/Profiletem";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
 class OrderDetailScreen extends Component {
+
 
   render() {
 
     return (
       <View style={styles.container}>
-        <Text>Order detail wana wane</Text>
-      
+
+        <View style={styles.row}>
+          <View style={styles.headerRow}>
+            <Ionicons name='ios-clock' size={40} color='#f58634'></Ionicons>
+            <Text style={styles.itemText} >00:28 pm</Text>
+          </View>
+          <View style={styles.buttonPadding}>
+            <Button type='outline' buttonStyle={styles.button} onPress={() => {
+              console.log('hello');
+            }} title='Confirm' titleStyle={styles.fonts}></Button>
+          </View>
+
+
+        </View>
+        <View style={styles.divider}>
+          <Divider ></Divider>
+        </View>
+        <Card>
+          <Text style={styles.cardBodyText} >You have 30 Minutes to confirm the avilability of the good</Text>
+        </Card>
+
+
+        <Card>
+          <View style={styles.row}>
+            <Text style={styles.cardHearderText} >Order Details</Text>
+            <Text style={styles.cardHearderText} > #2343</Text>
+          </View>
+          <Divider></Divider>
+          <View style={styles.row}>
+            <Text style={styles.cardBodyText2} >Item Name</Text>
+            <Text style={styles.cardBodyText2} > Sumsang TV</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.cardBodyText2} >Price</Text>
+            <Text style={styles.cardBodyText2} >TZ 2,000,000</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.cardBodyText2} >Size</Text>
+            <Text style={styles.cardBodyText2} >40 Inchs</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.cardBodyText2} >Color</Text>
+            <Text style={styles.cardBodyText2} >Black</Text>
+          </View>
+        </Card>
+
+
+
+
       </View>
     );
   }
--- src/screens/OrderDetail/styles.ts
+++ src/screens/OrderDetail/styles.ts
@@ -2,12 +2,87 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "space-evenly",
-    alignItems: "center"
+    flex: 1
   },
-  icon: {
-    paddingLeft: 10
+  separator: {
+    height: 1,
+    flex: 1,
+    backgroundColor: "gray"
+  },
+  header: {
+    color: "#f58634",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 30,
+    lineHeight: 18,
+    paddingTop: 40,
+    paddingLeft: 18,
+    marginBottom: 8,
+  },
+  row: {
+    flexDirection: "row",
+    padding: 10,
+    justifyContent: "space-between",
+   
+
+  },
+  headerRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  buttonPadding: {
+    paddingTop: 10,
+  },
+  itemText: {
+    paddingTop: 10,
+    paddingLeft: 10,
+    color: "#f58634",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  divider: {
+    paddingLeft: 10,
+    paddingRight: 10,
+
+  },
+  button: {
+    borderColor: "#f58634",
+    borderRadius: 10,
+    padding: 0,
+    paddingLeft: 10,
+    paddingRight: 10,
+    lineHeight: 14,
+
+
+  },
+  card: {
+    borderColor: "#f58634",
+    borderRadius: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  cardBodyText: {
+    padding: 10,
+    fontStyle: "normal",
+    fontSize: 15,
+  },
+  cardBodyText2: {
+    padding: 10,
+    fontStyle: "normal",
+    fontSize: 15,
+    color: "#7E7D7B",
+  },
+  cardHearderText: {
+   
+    color: "#f58634",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  fonts: {
+    color: "#f58634",
+    fontSize: 14,
   }
 });
 
